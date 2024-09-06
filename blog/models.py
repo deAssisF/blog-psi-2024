d2df22008e48a76b8dc2fad536a2bf85bec6e425
Francisco Cledson <franciscocledsof@gmail.com>
2024-09-06 10:20:13 -0300

    Mudanças

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Post(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=600)
+    imagem = models.ImageField(blank=True)
+    def __str__(self) -> str:
+        return self.titulo
 # Create your models here.
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def blog(request):
+    posts = Post.objects.all()
+
+    context ={
+        "posts": posts,
+    }
+
+    return render(request, "blog.html", context)
+ 
+def index(request):
+    return render(request, "index.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def post(request):
+    return render(request, "post.html")
